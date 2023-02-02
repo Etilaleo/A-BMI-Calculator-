7e3e899b978449d4f570389884a53aa7e7083b52
leo <108675763+Etilaleo@users.noreply.github.com>
2023-02-02 02:41:04 +0100

    BMI

--- lib/widgets/left.dart
+++ lib/widgets/left.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 class LeftScreen extends StatelessWidget {
-  const LeftScreen ({Key? key}) : super(key: key);
+  const LeftScreen ({Key? key, required this.barWidth}) : super(key: key);
+  final double barWidth;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    //We are returning a row because it to be in a horizontal order.
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          //Now we decorate the container
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomLeft: Radius.circular(20),
+            ),
+            color: Colors.yellowAccent
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widgets/right.dart
+++ lib/widgets/right.dart
@@ -1,2 +1,29 @@
+import 'package:flutter/material.dart';
 
+class RightScreen extends StatelessWidget {
+  const RightScreen ({Key? key, required this.barWidth}) : super(key: key);
+  final double barWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    //We are returning a row because it to be in a horizontal order.
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          //Now we decorate the container
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              color: Colors.yellowAccent
+          ),
+        ),
+      ],
+    );
+  }
+}
 
